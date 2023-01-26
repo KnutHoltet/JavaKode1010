b9cb83aab96688e6fb7eadac405e8fe7bc05e720
KnutHoltet <Knutholtet@live.no>
2023-01-26 18:04:06 +0100

    endret litt

--- TrixOppgaver/1/Baathus/src/TestBaathus.java
+++ TrixOppgaver/1/Baathus/src/TestBaathus.java
@@ -1,8 +1,10 @@
 public class TestBaathus {
     public static void main(String[] args) {
         Baathus baathus = new Baathus(2);
-        Baat baat = new Baat("LigmaBaats");
+        Baat baat = new Baat("Baat1");
+        Baat baat2 = new Baat("Baat2");
         baathus.settInn(baat);
+        baathus.settInn(baat2);
         baathus.skrivBaater();
     }
 }
